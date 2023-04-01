a9e2204fd471bbcc28bf9bba4245f1c391b4aa0e
David <david.owuor@student.moringaschool.com>
2023-04-01 07:47:22 +0300

    complete

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,7 @@
+
+
 class ItemsController < ApplicationController
+  def index
+      render json: Item.all, include: {user: { except: [:updated_at, :created_at] } }, except: [:updated_at, :created_at, :user_id]
+  end
 end
